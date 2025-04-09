588aa1caf011a44ff851d0366d6f0b55c09e8558
Shindoril <deniskashin1004@gmail.com>
2025-04-09 11:14:47 +0500

    add start second assignment

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,50 @@
+import assignment_2.MyArrayList;
 
+import java.util.ArrayList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        MyArrayList<Integer> list = new MyArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        print(list);
+        System.out.println("___________________________________________________");
+
+        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
+
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(5);
+        linkedList.add(6);
+
+        print(linkedList);
+
+
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println("______________________________________");
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
 
     }
+
+    public static void print(MyList<Integer> list) {
+        for (int i =0; i<list.size(); i++){
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+    }
+
 }
---
+++ src/assignment_2/MyLinkedList.java
@@ -0,0 +1,131 @@
+package assignment_2;
+
+import java.util.Iterator;
+
+public class MyLinkedList<T> implements MyList<T>{
+    private MyNode<T> node;
+    private int length;
+    public MyLinkedList(){
+        node = null;
+        length = 0;
+    }
+
+    @Override
+    public void add(T element) {
+        MyNode<T> newNode = new MyNode<>(element);
+        if(node==null){
+            node = newNode;
+        }
+        else{
+            MyNode<T> current = node;
+            while (current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+
+        }
+        length++;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void removeFirst() {
+
+    }
+
+    @Override
+    public void removeLast() {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        return 0;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void aadd(T item) {
+        
+    }
+
+    @Override
+    public void set(int index, T item) {
+
+    }
+
+    @Override
+    public void add(int index, T item) {
+
+    }
+
+    @Override
+    public void addFirst(T item) {
+
+    }
+
+    @Override
+    public void addLast(T item) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        if(index<0 || index>=size()){
+            throw new IndexOutOfBoundsException();
+        }
+        MyNode<T> current = node;
+        for(int i=0;i<index;i++){
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    @Override
+    public T getFirst() {
+        return null;
+    }
+
+    @Override
+    public T getLast() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return length;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
